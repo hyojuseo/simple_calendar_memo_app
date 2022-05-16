16df6d771138c6def30e559d0dbe89fac6d002f2
seohyoju <kinnylovemy@gmail.com>
2022-05-16 20:44:00 +0900

    TodoController add

--- lib/binding/init_bindings.dart
+++ lib/binding/init_bindings.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:todo_calendar/controller/bottom_navi_controller.dart';
 import 'package:todo_calendar/controller/calendar_controller.dart';
+import 'package:todo_calendar/controller/todo_controller.dart';
 
 class InitBindings extends Bindings{
   @override
   void dependencies() {
     Get.put(BottomNavController(), permanent: true);
     Get.put(CalendarController(), permanent: true);
+    Get.put(TodoController(), permanent: true);
   }
 }
